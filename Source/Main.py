f1423e8ef5baee0013ae27af56c4b5a8e89841a5
Laurie Brown <laurie@indieskies.com>
2017-05-19 00:02:13 +0100

    cleaned up file

--- Source/Main.py
+++ Source/Main.py
@@ -1,18 +1,4 @@
 # Actions Dictionary
 actions = {'inspect': 1, 'go': 2, 'use': 3, 'pick up': 4,}
-print(actions['inspect'])
 
-print("Hello Client")
-print('oioi')
-
-print("Hello there")
-
-name = input("What's your name? ")
-print("Nice to meet you " + name + "!")
-
-
-
-
-# Actions Dictionary
-actions = {'inspect': 1, 'go': 2, 'use': 3, 'pick up': 4,}
-print(actions['inspect'])
+playerInput = input("What do you want to do? ")
