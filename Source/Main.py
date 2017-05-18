b1cbec9fb5861f5a929d6a34adaf8cef452a4999
Unknown <hasselmyr@hotmail.com>
2017-05-18 23:58:43 +0100

    added action dictionary

--- Source/Main.py
+++ Source/Main.py
@@ -1,10 +1,18 @@
+# Actions Dictionary
+actions = {'inspect': 1, 'go': 2, 'use': 3, 'pick up': 4,}
+print(actions['inspect'])
 
-<<<<<<< HEAD
 print("Hello Client")
 print('oioi')
-=======
+
 print("Hello there")
 
 name = input("What's your name? ")
 print("Nice to meet you " + name + "!")
->>>>>>> master
+
+
+
+
+# Actions Dictionary
+actions = {'inspect': 1, 'go': 2, 'use': 3, 'pick up': 4,}
+print(actions['inspect'])
