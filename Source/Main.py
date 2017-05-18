ac8baaebead24c3b02469b75364aadb86f81cab4
Unknown <hasselmyr@hotmail.com>
2017-05-19 00:41:58 +0100

    FINISHED GAME

--- Source/Main.py
+++ Source/Main.py
@@ -1,4 +1,12 @@
 # Actions Dictionary
 actions = {'inspect': 1, 'go': 2, 'use': 3, 'pick up': 4,}
 
+
 playerInput = input("What do you want to do? ")
+WasFound = False
+for key in actions:
+    if key in playerInput:
+        WasFound = True
+        print ('you ' + key)
+if WasFound == False:
+    print ('I DO NOT UNDERSTAND')
